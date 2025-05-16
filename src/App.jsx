86aa2296383edb2ccf334c70c1cc3791c62dabd6
cloutxcoin <DAWNGOSSELIN80@GMAIL.COM>
2025-05-16 15:03:34 -0500

    final

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+function App() {
+  return (
+    <div style={{
+      backgroundColor: 'black',
+      color: 'lime',
+      fontFamily: 'monospace',
+      textAlign: 'center',
+      padding: '50px'
+    }}>
+      <h1>$PIMPJUICE Presale</h1>
+      <p>Wallet Connect + Buy Flow Ready</p>
+    </div>
+  )
+}
+
+export default App
